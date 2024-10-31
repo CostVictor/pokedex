31e68d654b77f193e5426f29e11925cd3a15a744
Cost_Victor <victorcost436@gmail.com>
2024-10-30 21:58:04 -0300

    feat: componente container

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardInfoComponent } from './card-info/card-info.component';
+import { ContainerComponent } from './container/container.component';
 
 
 
 @NgModule({
   declarations: [
-    CardInfoComponent
+    CardInfoComponent,
+    ContainerComponent
   ],
-  exports: [CardInfoComponent],
+  exports: [CardInfoComponent, ContainerComponent],
   imports: [
     CommonModule
   ]
